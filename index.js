4ed934c9b74122ee5f1213f86ad078c1a71502c6
Maliha-Amir <154528826+Maliha-Amir@users.noreply.github.com>
2024-07-26 20:01:05 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+let bar = document.getElementById('bar');
+let close = document.getElementById('close');
+let nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+
+let MainImg = document.getElementById("MainImg");
+let smallimg = document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function () {
+    MainImg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function () {
+    MainImg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function () {
+    MainImg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function () {
+    MainImg.src = smallimg[3].src;
+}
+
+
